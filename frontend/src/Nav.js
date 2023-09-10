c9719acf93d2a5dafcd0ea2152923e0ec2f042e3
baluyotkaylamae <126171614+baluyotkaylamae@users.noreply.github.com>
2023-09-10 17:53:52 +0800

    Update Nav.js

--- frontend/src/Nav.js
+++ frontend/src/Nav.js
@@ -1,22 +1,25 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { Link } from 'react-router-dom'
 
-
-const Nav =() => {
+const Nav = () => {
     return (
-        <nav>
-            <ul className="nav nav-tabs">
-                <li className="nav-item pr-3 pt-3 pb-3">
-                    <Link to="/">Home</Link>
-                </li>
-                <li className="nav-item pr-3 pt-3 pb-3">
-                    <Link to="/Create">Create</Link>
-                </li>
-
-
-            </ul>
+        <nav className='navbar sticky-top navbar-expand-lg navbar-dark bg-primary'>
+            <a className="navbar-brand" href="#"></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarText">
+                <ul className='navbar-nav'>
+                    <li className='nav-item'>
+                        <Link className='nav-link' to="/">Home</Link>
+                    </li>
+                    <li className='nav-item'>
+                        <Link className='nav-link' to="/create">Create</Link>
+                    </li>
+                </ul>
+            </div>
         </nav>
-
     )
 }
-export default Nav;
+
+export default Nav
