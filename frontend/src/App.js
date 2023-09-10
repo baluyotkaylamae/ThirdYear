685f1c6ea926d6d78d88a3f1a22eae475b968981
baluyotkaylamae <126171614+baluyotkaylamae@users.noreply.github.com>
2023-09-10 17:52:16 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,66 +1,69 @@
-import logo from './logo.svg';
+// utilities
+import { useEffect, useState } from 'react';
+import axios from 'axios'
+
+// static
 import './App.css';
-import Footer from './Footer'; // Footer.js
-// import Create from './Create'; //Create.j
-import React, { useEffect, useState } from "react";
-import Nav from './Nav'; // Import your Nav component
-import axios from 'axios';
-// import RoutedApp from './Routes'; // Import your routing component
+
+// components
+import Nav from './Nav';
+import Footer from './Footer'
+// import Create from './Create';
 
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([])
 
-  const [posts, setPosts] = useState([]);
   const fetchPosts = () => {
-    axios
-      .get('http://localhost:8000/api/posts')
-      .then((response) => {
-        console.log(response.data);
-        setPosts(response.data);
+    axios.get('http://127.0.0.1:8000/api/posts')
+      .then(res => {
+        // console.log(res.data.posts);
+        setPosts(res.data.posts)
       })
-      .catch((error) => {
-        console.error('Error fetching:', error);
+      .catch((err) => {
+        console.log(err);
       });
-  };
-  useEffect(() => {
-    fetchPosts();
-  }, []);
+  }
+
+  useEffect(() => { fetchPosts(); }, [])
+
+  const handleDelete = e => {
+    const id = e.target.getAttribute('data-id')
+    if (window.confirm('sure ka?')) {
+      alert('OKAY')
+      axios
+        .delete(`http://localhost:8000/api/post/${id}`, {
+        })
+        .then((res) => {
+          console.log(res);
+          fetchPosts();
+        })
+        .catch((err) => {
+          console.log(err);
+          // alert("error occured: " + err.response.data.message)
+        })
+      // .finally(() => {
+
+      // })
+    }
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-        <div className="App">
-          <Nav />
-          {Array.isArray(posts) ? (
-            posts.map((post, i) => (
-              <div key={i}>
-                <h2>{post.title}</h2>
-                <p>{post.slug}</p>
-                <p>{post.content}</p>
-              </div>
-            ))
-          ) : (
-            <p>No posts available</p>
-          )}
-        </div>
-
-
-
-        <Footer />
-      </header>
+    <div className="App" >
+      <Nav />
+      <div className='container p-xxl-5 p-sm-5'>
+        {posts.map((post) => (
+          <div key={post.id} style={{ marginBottom: "50px" }}>
+            <h3>{post.title}</h3>
+            <p>{post.content}</p>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button className='btn btn-outline-primary' style={{ marginRight: "10px" }} >Edit</button>
+              <button data-id={post.id} onClick={handleDelete} className='btn btn-outline-danger'>Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
   );
 }
