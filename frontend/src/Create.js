cc1c1614a0b23a410470f79d8db2a15793431f9d
baluyotkaylamae <126171614+baluyotkaylamae@users.noreply.github.com>
2023-09-10 17:52:52 +0800

    Update Create.js

--- frontend/src/Create.js
+++ frontend/src/Create.js
@@ -1,84 +1,94 @@
 import React, { useState } from 'react'
-
+import Nav from './Nav';
+import Footer from './Footer'
 import axios from 'axios';
 
-const Create = () => {
-    const [state, setState] = useState({
-      title: '',
-      content: '',
-      slug: ''
-    });
-  
-    const { title, content, slug } = state;
-  
-    const handleChange = (name) => (event) => {
-      setState({ ...state, [name]: event.target.value });
-    };
 
-    const handleSubmit = (event) => {
-        event.preventDefault(); // Prevent the default form submission behavior
-    
-        // Send the form data to your server using axios or another HTTP library
+const Create = () => {
+
+    const styles = {
+        width: "50%",
+        // minWidth: "400px"
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        console.log(title, content, user)
+
         axios
-          .post('http://localhost:8000/api/post', {
-            title,
-            content,
-            slug
-          })
-          .then((response) => {
-            // Handle the response from the server if needed
-            console.log('Post created successfully:', response.data);
-    
-            // Optionally, reset the form inputs to their initial state
-            setState({
-              title: '',
-              content: '',
-              slug: ''
-            });
-          })
-          .catch((error) => {
-            console.error('Error creating post:', error);
-          });
-      };
+            .post('http://127.0.0.1:8000/api/posts', state)
+            .then((res) => {
+                console.log(res.data);
+                setState({
+                    title: '',
+                    content: '',
+                    user: '',
+                    slug: "",
+                })
+            })
+            .catch((err) => {
+                console.log(err.response.data.message);
+            })
+    }
+
+    const [state, setState] = useState({
+        title: '',
+        content: '',
+        user: '',
+        slug: '',
+    });
+
+    const { title, content, user, slug } = state;
+
+    const handleChange = name => event => {
+        // console.log('name', name, 'event', event.target.value);
+        setState({ ...state, [name]: event.target.value });
+    }
+
 
     return (
-        <div>
-            <h1>Create Post</h1>
-            <br/>
-            <form onSubmit={handleSubmit}>
-
-                <div className="form-group">
-                    <label className='text-muted'>Title</label>
-                    <input type="text" 
-                    className='form-control' 
-                    placeholder='Post Title' required
-                    onChange={handleChange('title')} 
-                    value={title}
-                    />
-                </div>
-
-                <div className='form-group'>
-                    <label className='text-muted'>Content</label>
-                    <textarea type="text" 
-                    className='form-control' 
-                    placeholder='Write Something...' required 
-                    onChange={handleChange('content')} 
-                    value={content}
-                    />
-                </div>
-                <div className="form-group">
-                    <label className='text-muted'>Slug</label>
-                    <input type="text" 
-                    className='form-control' 
-                    placeholder='Slug' required 
-                      onChange={handleChange('slug')} 
-                      value={slug}/>
-                </div>
-                <div>
-                    <button className='btn btn-primary' type='submit'>Create</button>
-                </div>
-            </form>
-        </div>
+        <>
+            <Nav />
+            <div className="container p-xxl-5 p-sm-5" style={styles}>
+                <h1 style={{ textAlign: "center" }}> Create</h1>
+                <form onSubmit={handleSubmit}>
+                    <div className='form-group'>
+                        <label className='text-muted'>Title</label>
+                        <input type='text'
+                            className='form-control'
+                            value={title}
+                            onChange={handleChange('title')}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label className='text-muted'>Content</label>
+                        <textarea type='text'
+                            className='form-control'
+                            value={content}
+                            onChange={handleChange('content')}
+                        > </textarea>
+                    </div>
+                    <div className='form-group'>
+                        <label className='text-muted'>Slug</label>
+                        <input type='text'
+                            className='form-control'
+                            value={slug}
+                            onChange={handleChange('slug')}
+                        />
+                    </div>
+                    <div className='form-group'>
+                        <label className='text-muted'>User</label>
+                        <input type='text'
+                            className='form-control'
+                            value={user}
+                            onChange={handleChange('user')}
+                        />
+                    </div>
+                    <button style={{ marginTop: "20px" }} type='submit' className='btn btn-primary'>Submit</button>
+                </form>
+            </div >
+            <Footer />
+        </>
     )
 }
 
