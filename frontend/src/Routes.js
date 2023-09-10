8285d1b6c2deee0e9326d242efe383ddcc0f208f
baluyotkaylamae <126171614+baluyotkaylamae@users.noreply.github.com>
2023-09-10 18:14:37 +0800

    Update Routes.js

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,17 +1,19 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Create from './Create';
-import App from './App';
-const RoutedApp =() => {
+import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
+
+import Create from './Create'
+import App from './App'
+
+const RoutedApp = () => {
     return (
         <Router>
             <Routes>
-            <Route path="/" exact="true" element={<App />} />
-            <Route path="/Create" exact="true" element={<Create />} />
-
+                <Route path='/' exact="true" element={<App />} />
+                <Route path='/create' exact="true" element={<Create />} />
+                
             </Routes>
         </Router>
-    ) 
+    )
 }
 
 export default RoutedApp
